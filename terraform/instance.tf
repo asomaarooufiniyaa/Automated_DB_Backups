351050e8894130aa4c888c237ba4576741304308
asomaarooufiniyaa <asomaarooufiniyaa@gmail.com>
2025-11-05 20:09:31 +0330

    docker compose and EBS valume added

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -9,3 +9,18 @@ resource "aws_instance" "web_server" {
     Name = "web_server"
   }
 }
+resource "aws_ebs_volume" "mongo_data" {
+  availability_zone = aws_instance.web_server[0].availability_zone
+  size              = 10       
+  type              = "gp3"  
+
+  tags = {
+    Name = "mongo-data-volume"
+  }
+}
+resource "aws_volume_attachment" "mongo_attach" {
+  device_name = "/dev/xvdf"                
+  volume_id   = aws_ebs_volume.mongo_data.id
+  instance_id = aws_instance.web_server[0].id
+  force_detach = true
+}
